674162fd5c5e626a91b64a58153f1bb0bc611149
Mary Paul <marymc135@hotmail.com>
2022-07-16 13:51:17 -0400

    error function

--- homepage.js
+++ homepage.js
@@ -9,9 +9,17 @@ var apiUrl = "https://api.github.com/users/" + user + "/repos";
      
 //make a request to the url
 fetch(apiUrl).then (function(response) {
+    if (response.ok) {
     response.json().then(function(data) {
         displayRepos(data, user);
       });
+    } else {
+        alert ("Error:GitHub User Not Found");
+    }
+     })
+     .catch(function(error){
+        //Notice this '.catch()' getting chained onto the end of the '.then()' method
+        alert("Unable to connect to GitHub");
      });
     }
 
@@ -31,6 +39,11 @@ var formSubmitHandler = function(event) {
 
 var displayRepos = function(repos, searchTerm) {
     //clear old content
+    //check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
